f7cef5308db0e906b42d1ff6288109051c4d1cb5
BackOfficeSolutions <cons.manzano@backofficesolutions.ph>
2019-04-25 19:00:33 +0800

    092519

--- FinancialSystem/Accessor/Users/LoginAccessor.cs
+++ FinancialSystem/Accessor/Users/LoginAccessor.cs
@@ -1,5 +1,6 @@
 ﻿using FinancialSystem.Controllers;
 using FinancialSystem.Models;
+using FinancialSystem.Utilities;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,10 @@ namespace FinancialSystem.Accessor.Users {
 			
 			if (ModelState.IsValid) {
 				var user = await UserManager.FindAsync(model.UserName.ToLower(), model.Password);
+				if (user != null) {
+					await SignInAsync(user, model.RememberMe);
+					var usr = HibernateSession.GetCurrentSession();
+				}
 				return user;
 			}
 			return null;
