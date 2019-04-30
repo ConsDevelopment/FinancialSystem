8b1ad2c8e9808db966ea2ed991f532a2c94d338d
ryanespina <43159176+ryanespina@users.noreply.github.com>
2019-04-30 14:12:12 +0800

    043019

---
+++ FinancialSystem/Controllers/MVC/Maintenace/MaintenanceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinancialSystem.Controllers.MVC.Maintenace
+{
+    public class MaintenanceController : Controller
+    {
+        // GET: Maintenance
+        public ActionResult AddUsers()
+        {
+            return View();
+        }
+    }
+}
