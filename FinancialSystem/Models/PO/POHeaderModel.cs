4ec75b89be86022f5c2ceebc9e9e44996e2c39b9
BackOfficeSolutions <cons.manzano@backofficesolutions.ph>
2019-10-09 19:08:24 +0800

    100919

--- FinancialSystem/Models/PO/POHeaderModel.cs
+++ FinancialSystem/Models/PO/POHeaderModel.cs
@@ -10,13 +10,15 @@ using System.Threading.Tasks;
 using FinancialSystem.NHibernate;
 
 namespace FinancialSystem.Models {
-	public class PRHeaderModel {
+	public class POHeaderModel {
 		private string requisitionNo;
 		public virtual long Id { get; set; }
-		public virtual StatusType Status { get; set; }
+		public virtual SupplierModel Supplier { get; set; }
+		public virtual PaymentTermsType PaymentTerm { get; set; }
 		public virtual EmployeeModel Requestor { get; set; }
 		public virtual string DeliveryAdress { get; set; }
-		public virtual ChargeLocationModel CLC { get; set; }
+		public virtual StatusType Status { get; set; }
+		public virtual DateTime? RequiredDate { get; set; }
 
 		public virtual string RequisitionNo {
 			get {
@@ -30,38 +32,34 @@ namespace FinancialSystem.Models {
 				requisitionNo = value;
 			}
 		}
-		public virtual DateTime? DateNeeded { get; set;  }
-		public virtual CostRevenueCenterModel CRC { get; set; }
-		public virtual UserModel CreatedBy { get; set; }
-		public virtual string CategoryAccountCode { get; set; }
+		public virtual CostRevenueCenterModel CRC { get; set; }		
 		public virtual double Amount { get; set; }
-		public virtual ICollection<PRLinesModel> Lines { get; set; }
-		public virtual ICollection<PRAprovalModel> Approvals { get; set; }
 
-		public PRHeaderModel() {			
+		public virtual ICollection<PRLinesModel> Lines { get; set; }
+
+		public POHeaderModel() {			
 			CreateTime = DateTime.UtcNow;			
 		}
-		
+		public virtual UserModel CreatedBy { get; set; }
 		public virtual DateTime CreateTime { get; set; }
 		public virtual DateTime? DeleteTime { get; set; }
 
-		public class PRHeaderModelMap : ClassMap<PRHeaderModel> {
-			public PRHeaderModelMap() {
+		public class POHeaderModelMap : ClassMap<POHeaderModel> {
+			public POHeaderModelMap() {
 				Id(x => x.Id);
-				Map(x => x.Status).Length(30);
+				Map(x => x.PaymentTerm).Length(20);
 				Map(x => x.DeliveryAdress);
+				Map(x => x.Status).Length(20);
+				Map(x => x.RequiredDate);
 				Map(x => x.RequisitionNo);
-				Map(x => x.DateNeeded);
-				Map(x => x.CategoryAccountCode);
-				Map(x => x.DeleteTime);
-				Map(x => x.CreateTime);
 				Map(x => x.Amount);
-				References(x => x.CreatedBy, "CreatedBy").Cascade.SaveUpdate();
-				References(x => x.CLC, "CLC").Cascade.SaveUpdate();
-				References(x => x.CRC, "CRC").Cascade.SaveUpdate();
+				Map(x => x.CreateTime);
+				Map(x => x.DeleteTime);
+				References(x => x.Supplier, "Supplier").Cascade.SaveUpdate();
 				References(x => x.Requestor, "Requestor").Cascade.SaveUpdate();
+				References(x => x.CRC, "CRC").Cascade.SaveUpdate();
 				HasMany(x => x.Lines).Cascade.SaveUpdate().KeyColumn("Header");
-				HasMany(x => x.Approvals).Cascade.SaveUpdate().KeyColumn("PRHeader");
+				References(x => x.CreatedBy, "CreatedBy").Cascade.SaveUpdate();
 			}
 		}
 
