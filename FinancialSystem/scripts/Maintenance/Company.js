659dccf28503aeb3f236a15e9874bdb611ebfb5b
cons <cons@mytractiontools.com>
2020-01-22 00:41:03 +0800

    1-22-20

--- FinancialSystem/scripts/Maintenance/Company.js
+++ FinancialSystem/scripts/Maintenance/Company.js
@@ -7,5 +7,19 @@ function AddCompany(elmnt) {
 	RoeElem = elmnt;
 }
 function AddingCompany() {
-
+	alert('test');
+	var data = new FormData();
+	var logo = $(RoeElem).closest("tr").find("td:eq(1) input[type='file']")[0].files[0];
+	data.append('file', logo);
+	$.ajax({
+		url: '/api/AddLog',
+		processData: false,
+		contentType: false,
+		data: data,
+		type: 'POST'
+	}).done(function (result) {
+		alert(result);
+	}).fail(function (a, b, c) {
+		console.log(a, b, c);
+	});
 }
